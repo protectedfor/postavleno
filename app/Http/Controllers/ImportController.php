6e28a8dc8f09e0475db96dfc8b81649ee32762a7
Andrey Fedotov <protected.for@gmail.com>
2023-11-15 23:43:49 +0600

    replaced with upsert

--- app/Http/Controllers/ImportController.php
+++ app/Http/Controllers/ImportController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Http;
 
@@ -13,28 +14,26 @@ class ImportController extends Controller
         $response = Http::get('https://randomuser.me/api/?results=5000');
 
         $users = $response->collect('results')->chunk(500);
-        $added_count = 0;
-        $updated_count = 0;
+        $startedAt = Carbon::now();
+
         foreach ($users as $chunk) {
+            $data = [];
             foreach ($chunk as $user) {
-                $db_user = User::updateOrCreate([
+                $data[] = [
                     'first_name' => data_get($user, 'name.first'),
                     'last_name'  => data_get($user, 'name.last'),
-                ], [
                     'email' => data_get($user, 'email'),
-                    'age'   => data_get($user, 'dob.age')
-                ]);
-                // It will return true if the record have been modified
-                $wasChanged = $db_user->wasChanged();
-                if ($wasChanged)
-                    $updated_count++;
-                else
-                    $added_count++;
+                    'age'   => data_get($user, 'dob.age'),
+                ];
             }
+            User::upsert($data, ['first_name', 'last_name'], ['email', 'age']);
         }
 
+        $added_count = DB::table('users')->whereNull('password')->whereColumn('created_at', '=', 'updated_at')->where('created_at', '>=', $startedAt)->count();
+        $updated_count = DB::table('users')->whereNull('password')->whereColumn('created_at', '!=', 'updated_at')->where('updated_at', '>=', $startedAt)->count();
+
         return [
-            'total'   => DB::table('users')->count(),
+            'total'   => DB::table('users')->whereNull('password')->count(),
             'added'   => $added_count,
             'updated' => $updated_count,
         ];
