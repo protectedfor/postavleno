6e4856641784220e132115d2857926c9db44f161
Andrey Fedotov <protected.for@gmail.com>
2023-11-15 21:09:30 +0600

    removed comments

--- app/Http/Controllers/ImportController.php
+++ app/Http/Controllers/ImportController.php
@@ -10,36 +10,6 @@ class ImportController extends Controller
 {
     public function index()
     {
-//        DB::table('users')->where('id', '>', 1)->delete();
-//        die();
-
-//        $response = Http::get('https://randomuser.me/api/?results=500');
-//        $users = $response->collect('results')->chunk(100);
-//
-//        $added_count = 0;
-//        $updated_count = 0;
-//
-//        DB::transaction(function () use ($users, &$added_count, &$updated_count) {
-//            foreach ($users as $chunk) {
-//                $data = [];
-//
-//                foreach ($chunk as $user) {
-//                    $data[] = [
-//                        'first_name' => data_get($user, 'name.first'),
-//                        'last_name'  => data_get($user, 'name.last'),
-//                        'email'      => data_get($user, 'email'),
-//                        'age'        => data_get($user, 'dob.age'),
-//                    ];
-//                }
-//
-//                DB::table('users')->upsert($data, ['first_name', 'last_name'], ['email', 'age']);
-//                $added_count += count($data);
-//
-////                DB::table('users')->insertOrIgnore($data);
-////                $added_count += count($data);
-//            }
-//        });
-
         $response = Http::get('https://randomuser.me/api/?results=5000');
 
         $users = $response->collect('results')->chunk(500);
